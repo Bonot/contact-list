08ed49546765db0bfac99c268cd0c712e3f5cbf9
bonot <paulabonot@gmail.com>
2023-04-23 15:59:08 -0300

    Cria página para editar contatos

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import ContactsList from '../views/Contacts/List.vue'
 import ContactsCreate from '../views/Contacts/Create.vue'
+import ContactsEdit from '../views/Contacts/Edit.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/contatos/novo',
       name: 'ContactsCreate',
       component: ContactsCreate,
+    },
+    {
+      path: '/contatos/:id/editar',
+      name: 'ContactsEdit',
+      component: ContactsEdit,
     },
   ]
 })
---
+++ frontend/src/views/Contacts/Edit.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h4>Editar contato</h4>
+            </div>
+            <div class="card-body">
+                <ul class="alert alert-warning" v-if="Object.keys(this.errorList).length > 0">
+                    <li class="mb-0 ms-3" v-for="(error, index) in this.errorList" :key="index">
+                        {{ error[0] }}
+                    </li>
+                </ul>
+                <div class="mb-3">
+                    <label>Nome Completo</label>
+                    <input type="text" v-model="this.model.person.name" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <add-contacts :dataContacts="this.model.person.contacts"></add-contacts>
+                </div>
+            <div class="mb-3">
+                <RouterLink to="/contatos" class="btn btn-primary mx-2">Voltar</RouterLink>
+                <button type="button" @click="updateContact" class="btn btn-success">Salvar</button>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import addContacts from '../../components/addContacts.vue';
+import router from '../../router';
+
+export default {
+    name: 'contactCreate',
+    components: {
+        addContacts,
+    },
+    data(){
+        return {
+            errorList: '',
+            model: {
+                person: {
+                    name: '',
+                    contacts: [],
+                }
+            },
+        }
+    },
+    mounted() {
+        this.personId = this.$route.params.id;
+        this.getContact(this.personId);
+    },
+    methods: {
+        getContact(personId) {
+            axios.get(`/api/contacts-list/${personId}`)
+                .then(response => {
+                    this.model.person = response.data;
+                })
+                .catch(function (error) {
+                        alert('Houve um erro ao localizar esse contato')
+                        router.push('/contatos');
+                })
+        },
+        updateContact() {
+            axios.put(`/api/contacts-list/${this.personId}`, this.model.person)
+                .then(response => {
+                    if (response.data.success === false) {
+                        this.errorList = response.data.errors;
+                    }
+
+                    if (response.status === 201) {
+                        alert('Contato salvo com sucesso');
+                        router.push('/contatos');
+                    }
+                })
+        }
+    }
+}
+</script>
