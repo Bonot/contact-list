be12b3907f8579b7d48b09f959ad5b3c5ed801b4
bonot <paulabonot@gmail.com>
2023-04-23 20:16:16 -0300

    Exige autenticação para acessar as rotas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,7 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('contacts-list/contact-types', [ContactListController::class, 'getContactTypes']);
-Route::apiResource('/contacts-list', ContactListController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('contacts-list/contact-types', [ContactListController::class, 'getContactTypes']);
+    Route::apiResource('/contacts-list', ContactListController::class);
+});
 
 Route::post('/login', LoginController::class);
