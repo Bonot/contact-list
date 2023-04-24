97b246a06865203919e3b06b13176fc8d3cbae18
bonot <paulabonot@gmail.com>
2023-04-23 21:15:29 -0300

    Redireciona rotas quando usuário não logado

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import ContactsCreate from '../views/Contacts/Create.vue'
 import ContactsEdit from '../views/Contacts/Edit.vue'
 import ContactsView from '../views/Contacts/View.vue'
 import Login from '../views/Login.vue'
+import authorization from '../middlewares/authorization'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,31 +14,37 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login,
+      beforeEnter: authorization
     },
     {
       path: '/',
       name: 'home',
       component: Home,
+      beforeEnter: authorization,
     },
     {
       path: '/contatos',
       name: 'ContactsList',
       component: ContactsList,
+      beforeEnter: authorization,
     },
     {
       path: '/contatos/novo',
       name: 'ContactsCreate',
       component: ContactsCreate,
+      beforeEnter: authorization,
     },
     {
       path: '/contatos/:id/editar',
       name: 'ContactsEdit',
       component: ContactsEdit,
+      beforeEnter: authorization,
     },
     {
       path: '/contatos/:id/ver',
       name: 'ContactsView',
       component: ContactsView,
+      beforeEnter: authorization,
     },
   ]
 })
